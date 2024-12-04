fe175b47818b9ecabd6aff7a2a3cb648e3d6cacd
joachimageron <ageron.joachim@gmail.com>
2024-12-04 20:38:29 +0100

    ✨ add: implement dark mode switch component
    Introduce a DarkModeSwitch component to toggle between dark and light
    themes. This includes icons for sun and moon to represent the modes.
    Update NavigationBar to incorporate the new DarkModeSwitch.

--- app/components/NavigationBar.tsx
+++ app/components/NavigationBar.tsx
@@ -1,27 +1,32 @@
 import Link from "next/link";
 import HomeIcon from "./icons/HomeIcon";
 import SearchIcon from "./icons/SearchIcon";
-import {serverSession} from "@/utils/auth";
-import UserImage from "@/app/components/UserImage";
-import PostModal from "@/app/components/PostModal";
+import CreatePostButton from "@/app/components/CreatePostButton";
+import UserAvatarMenu from "@/app/components/UserAvataMenu";
+import DarkModeSwitch from "@/app/components/DarkModeSwitch";
+
 
 export default async function NavigationBar() {
-  const session = await serverSession();
   
   return (
-    <nav className={"fixed bottom-0 left-0 w-full h-12 flex justify-center gap-10 items-center bg-primary"}>
-      <Link href={"/"} className={"w-7"}>
-          <HomeIcon />
-      </Link>
-      <Link href={"search"} className={"w-7"}>
-          <SearchIcon />
-      </Link>
-      <PostModal/>
-      {session &&
-         <Link href={`/profil/${session.user.userId}`}>
-            <UserImage userImageUrl={session.user.image ?? ""} className={"rounded-full w-7"}/>
-         </Link>
-      }
+    <nav className={"fixed bottom-0 left-0 w-full h-12 bg-primary flex justify-center items-center z-30"}>
+      <div className={"flex justify-center gap-10 items-center"}>
+        <Link href={"/"} className={"w-7"}>
+          <HomeIcon className={"fill-zinc-50"}/>
+        </Link>
+        <Link href={"search"} className={"w-7"}>
+          <SearchIcon className={"fill-zinc-50"}/>
+        </Link>
+        <div className={"w-7"}>
+          <CreatePostButton/>
+        </div>
+        <div className={"w-7"}>
+          <UserAvatarMenu/>
+        </div>
+      </div>
+      <div className={"absolute right-0"}>
+        <DarkModeSwitch/>
+      </div>
     </nav>
   )
 }
