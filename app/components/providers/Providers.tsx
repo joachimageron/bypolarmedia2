8e5a8cec4be4f23e76dc60aaf6e0e1277f67d5a2
ageron <ageron.joachim@gmail.com>
2025-01-24 16:48:01 +0100

    ✨ add: integrate Vercel Analytics to Providers component
    Include @vercel/analytics package in the dependencies list in
    package.json. Integrate Analytics component from @vercel/analytics
    into Providers component in Providers.tsx file.

--- app/components/providers/Providers.tsx
+++ app/components/providers/Providers.tsx
@@ -3,18 +3,19 @@
 
 import {SessionProvider} from "next-auth/react";
 import {NextUIProvider} from "@nextui-org/system";
+import { Analytics } from "@vercel/analytics/react"
 import NotificationProvider from "./NotificationProvider"
 // import {useRouter} from 'next/navigation'
 
 export default function Providers({children}: Readonly<{
   children: React.ReactNode;
 }>) {
-  // const router = useRouter();
   
   return (
     <SessionProvider>
       <NextUIProvider>
         <NotificationProvider>
+          <Analytics/>
         {children}
         </NotificationProvider>
       </NextUIProvider>
